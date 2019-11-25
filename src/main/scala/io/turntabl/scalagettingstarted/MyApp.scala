8c5e0fa1cf20354fa488c2b3dc73020c8ec3d66c
patricia-agyekum <patriciakasapa07@gmail.com>
2019-11-25 13:52:42 +0000

    Labs for Hello Scala all done

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -1,5 +1,36 @@
 package io.turntabl.scalagettingstarted
 
 object MyApp extends App {
-    print("Hello, World!")
+  val firstname = "Patricia"
+  val town = "Kwahu Abetifi"
+  println(s"My name is $firstname, and i come from $town")
+
+  def threeTimes(i: String) = i * 3
+  println(threeTimes("we"))
+
+  def even(num: Int) = if(num % 2 == 0) print("even")
+ println(even(3))
+
+  def wordsToLower(words: String*) = words map(_.toLowerCase)
+ wordsToLower("TabiA", "MaLLam", "TarKoSt")
+
+  def even2(num: Int)= num % 2 match{
+   case 0 => true
+   case 1 => false
+  }
+  even2(3)
+
+ def isWeekendDay(oneday: String) =
+  oneday match {
+   case "Saturday" | "Sunday" => true
+   case _ => false
+  }
+
+ def areWeekendDays(days: String*) = days map (justday => isWeekendDay(justday))
+
+ areWeekendDays("Monday", "Saturday") foreach println
+
+ def stringPrint(s: String, i: Int) = s * i
+ println(stringPrint("Malam", 4))
 }
+
